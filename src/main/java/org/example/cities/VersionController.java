b743c0eba0dc22eebf8dc6ccae68c3d998d671c7
Scott Frederick <scottyfred@gmail.com>
2014-10-09 09:07:45 -0500

    Updated greeting.

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Dallas Cloud Foundry Roadshow!";
+        return "Greetings Cloud Foundry Roadshow!";
     }
 }
